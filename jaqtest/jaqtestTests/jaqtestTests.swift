69cac4664cee4c15b4b9e406cb6748f59b2a4dbb
jaqlai <jaqlai888@gmail.com>
2020-02-25 14:40:51 -0500

    commit
    added magic

---
+++ jaqtest/jaqtestTests/jaqtestTests.swift
@@ -0,0 +1,34 @@
+//
+//  jaqtestTests.swift
+//  jaqtestTests
+//
+//  Created by JaQ Lai on 2/20/20.
+//  Copyright © 2020 CS590. All rights reserved.
+//
+
+import XCTest
+@testable import jaqtest
+
+class jaqtestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
